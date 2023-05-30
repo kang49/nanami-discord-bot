3c32286708ff19acc88c901aa8a9d498a26bafd0
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-05-30 21:16:29 +0700

    (Command): Delete Msg

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,7 +1,26 @@
 import type client from '../index';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export = (client: client) => {
-  client.on('messageCreate', async (message) => {
-  
-  });
+    client.on('messageCreate', async (message) => {
+        const guildId: string = message.guildId as string
+        const channelId: string = message.channelId as string
+        const messageId: string = message.id as string
+        const messageAuthorId: string = message.author.id
+
+        try {
+            await prisma.deleteMessage.create({
+                data: {
+                    guildId: guildId,
+                    channelId: channelId,
+                    messageId: messageId,
+                    messageAuthorId: messageAuthorId,
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            return;
+        }
+    });
 };
