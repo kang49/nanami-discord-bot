466e9e6e96d664d93069001c3da07edec0db7b4e
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-07-08 19:44:03 +0700

    (Bug, Func): Nitro badge not show welcome fixed2

--- src/events/welcomeMsg.ts
+++ src/events/welcomeMsg.ts
@@ -6,6 +6,7 @@ const Discord = require('discord.js');
 import { ChannelType} from 'discord.js';
 
 import { createCanvas, loadImage } from 'canvas';
+const axios = require('axios');
 
 export = (client: client) => {
     client.on('guildMemberAdd', async (member) => {
@@ -32,137 +33,148 @@ export = (client: client) => {
         //get png avatar
         userAvatar = userAvatar.replace('.webp', '.png');
 
-        // Text to be centered
-        if (discriminator === '#0') {discriminator = ''};
-        const displayName_prepare = `${displayName}${discriminator}`;
-
-        
-        // Create canvas
-        const canvas = createCanvas(800, 600);
-        const ctx = canvas.getContext('2d');
-
-        // Load image
-        loadImage('assets/img/nanamiWelcome.png').then ( async (image) => {
-            // Set canvas size
-            canvas.width = image.width;
-            canvas.height = image.height;
-
-            // Draw image
-            ctx.drawImage(image, 0, 0);
-
-            // Set text color and font
-            ctx.fillStyle = '#3a9ff4';
-            ctx.font = 'bold 50px Arial';
-            // Draw text at the centered position
-            ctx.fillText(displayName_prepare, 310, 180);
-
-
-            const memberCount_prepare = `เข้ามาเป็นคนที่: ${memberCount}`;
-            ctx.fillStyle = '#3a9ff4';
-            ctx.font = 'bold 25px Arial';
-            // Draw text at the centered position
-            ctx.fillText(memberCount_prepare, 310, 220);
-
-            if (userFlags?.has('ActiveDeveloper')) {
-                //Badge
-                // Load image
-                const badgeImage = await loadImage('assets/img/active_developer_icon.png');
-                
-                // Calculate avatar position and radius
-                const badgeSize = 100;
-                const badgeX = 750;
-                const badgeY = 220;
-
-                // Draw avatar image within the clipping path
-                ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
-
-                ctx.fillStyle = 'white';
-                ctx.font = 'bold 25px Arial';
-                // Draw text at the centered position
-                ctx.fillText(`Discord Developer`, 520, 278);
-            } else if (userFlags?.has('PremiumEarlySupporter') || (userPremium)) {
-                //Badge
-                // Load image
-                const badgeImage = await loadImage('assets/img/nitro_icon.png');
-                
-                // Calculate avatar position and radius
-                const badgeSize = 100;
-                const badgeX = 750;
-                const badgeY = 220;
-
-                // Draw avatar image within the clipping path
-                ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
-
-                ctx.fillStyle = 'white';
-                ctx.font = 'bold 25px Arial';
-                // Draw text at the centered position
-                ctx.fillText(`Nitro Supporter`, 550, 278);
-            } else if (member.user.bot) {
-                //Badge
-                // Load image
-                const badgeImage = await loadImage('assets/img/bot_icon.png');
-                
-                // Calculate avatar position and radius
-                const badgeSize = 60;
-                const badgeX = 780;
-                const badgeY = 237;
-
-                // Draw avatar image within the clipping path
-                ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
-
-                ctx.fillStyle = 'white';
-                ctx.font = 'bold 25px Arial';
-                // Draw text at the centered position
-                ctx.fillText(`Discord`, 670, 278);
+        //Check UserBanner for check user has Nitro
+        axios.get(`https://discord.com/api/users/${member.user.id}`,
+            {
+                headers: {
+                    Authorization: `Bot ${client.token}`
+                }
             }
+        ).then((res:any) => {
+            const {banner} = res.data;
 
-            //Avatar
-            // Load avatar image
-            const avatar = await loadImage(userAvatar);
+            // Text to be centered
+            if (discriminator === '#0') {discriminator = ''};
+            const displayName_prepare = `${displayName}${discriminator}`;
+
             
-            // Calculate avatar position and radius
-            const avatarSize = 220;
-            const avatarX = 70;
-            const avatarY = 47;
-            const radius = avatarSize / 2;
+            // Create canvas
+            const canvas = createCanvas(800, 600);
+            const ctx = canvas.getContext('2d');
 
-            // Create clipping path for the circle
-            ctx.beginPath();
-            ctx.arc(avatarX + radius, avatarY + radius, radius, 0, Math.PI * 2, true);
-            ctx.closePath();
-            ctx.clip();
+            // Load image
+            loadImage('assets/img/nanamiWelcome.png').then ( async (image) => {
+                // Set canvas size
+                canvas.width = image.width;
+                canvas.height = image.height;
 
-            // Draw avatar image within the clipping path
-            ctx.drawImage(avatar, avatarX, avatarY, avatarSize, avatarSize);
+                // Draw image
+                ctx.drawImage(image, 0, 0);
 
-            // const userRank_TextWidth = ctx.measureText(displayName_prepare).width;
-            // Calculate X position for centering text
-            // const centerX = (canvas.width - displayName_canvas) / 2;
+                // Set text color and font
+                ctx.fillStyle = '#3a9ff4';
+                ctx.font = 'bold 50px Arial';
+                // Draw text at the centered position
+                ctx.fillText(displayName_prepare, 310, 180);
 
-            // Save image
-            const welcomeBuffer = canvas.toBuffer();
-            // fs.writeFileSync('assets/welcomeMsg.png', welcomeBuffer);
 
-            //Send
-            const attachment = new Discord.AttachmentBuilder(welcomeBuffer, { name: 'welcomeMsg.png' });
+                const memberCount_prepare = `เข้ามาเป็นคนที่: ${memberCount}`;
+                ctx.fillStyle = '#3a9ff4';
+                ctx.font = 'bold 25px Arial';
+                // Draw text at the centered position
+                ctx.fillText(memberCount_prepare, 310, 220);
 
-            _welcomeChannelId?.send({
-                embeds: [
-                    {
-                        color: 0x0099ff,
-                        title: `ยินดีต้อนรับเข้าสู่ ***${member.guild?.name}*** นะคะ`,
-                        description: `คุณ **${displayName_prepare}** ได้เข้าสู่ Server!!`,
-                        image: {
-                            url: `attachment://welcomeMsg.png`
-                        },
-                        footer: {
-                            text: `Nanami Welcome`
-                        },
-                        timestamp: new Date().toISOString(),
-                    }
-                ],
-                files: [attachment],
-            })
+                if (userFlags?.has('ActiveDeveloper')) {
+                    //Badge
+                    // Load image
+                    const badgeImage = await loadImage('assets/img/active_developer_icon.png');
+                    
+                    // Calculate avatar position and radius
+                    const badgeSize = 100;
+                    const badgeX = 750;
+                    const badgeY = 220;
+
+                    // Draw avatar image within the clipping path
+                    ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
+
+                    ctx.fillStyle = 'white';
+                    ctx.font = 'bold 25px Arial';
+                    // Draw text at the centered position
+                    ctx.fillText(`Discord Developer`, 520, 278);
+                } else if (userFlags?.has('PremiumEarlySupporter') || (userPremium) || (banner)) {
+                    //Badge
+                    // Load image
+                    const badgeImage = await loadImage('assets/img/nitro_icon.png');
+                    
+                    // Calculate avatar position and radius
+                    const badgeSize = 100;
+                    const badgeX = 750;
+                    const badgeY = 220;
+
+                    // Draw avatar image within the clipping path
+                    ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
+
+                    ctx.fillStyle = 'white';
+                    ctx.font = 'bold 25px Arial';
+                    // Draw text at the centered position
+                    ctx.fillText(`Nitro Supporter`, 550, 278);
+                } else if (member.user.bot) {
+                    //Badge
+                    // Load image
+                    const badgeImage = await loadImage('assets/img/bot_icon.png');
+                    
+                    // Calculate avatar position and radius
+                    const badgeSize = 60;
+                    const badgeX = 780;
+                    const badgeY = 237;
+
+                    // Draw avatar image within the clipping path
+                    ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
+
+                    ctx.fillStyle = 'white';
+                    ctx.font = 'bold 25px Arial';
+                    // Draw text at the centered position
+                    ctx.fillText(`Discord`, 670, 278);
+                }
+
+                //Avatar
+                // Load avatar image
+                const avatar = await loadImage(userAvatar);
+                
+                // Calculate avatar position and radius
+                const avatarSize = 220;
+                const avatarX = 70;
+                const avatarY = 47;
+                const radius = avatarSize / 2;
+
+                // Create clipping path for the circle
+                ctx.beginPath();
+                ctx.arc(avatarX + radius, avatarY + radius, radius, 0, Math.PI * 2, true);
+                ctx.closePath();
+                ctx.clip();
+
+                // Draw avatar image within the clipping path
+                ctx.drawImage(avatar, avatarX, avatarY, avatarSize, avatarSize);
+
+                // const userRank_TextWidth = ctx.measureText(displayName_prepare).width;
+                // Calculate X position for centering text
+                // const centerX = (canvas.width - displayName_canvas) / 2;
+
+                // Save image
+                const welcomeBuffer = canvas.toBuffer();
+                // fs.writeFileSync('assets/welcomeMsg.png', welcomeBuffer);
+
+                //Send
+                const attachment = new Discord.AttachmentBuilder(welcomeBuffer, { name: 'welcomeMsg.png' });
+
+                _welcomeChannelId?.send({
+                    embeds: [
+                        {
+                            color: 0x0099ff,
+                            title: `ยินดีต้อนรับเข้าสู่ ***${member.guild?.name}*** นะคะ`,
+                            description: `คุณ **${displayName_prepare}** ได้เข้าสู่ Server!!`,
+                            image: {
+                                url: `attachment://welcomeMsg.png`
+                            },
+                            footer: {
+                                text: `Nanami Welcome`
+                            },
+                            timestamp: new Date().toISOString(),
+                        }
+                    ],
+                    files: [attachment],
+                })
+            });
         });
-    });
+    })
 }
