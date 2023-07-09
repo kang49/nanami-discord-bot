d539fc4ed6d7a8d16fd2135dcf074fb89118f624
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-07-09 16:01:05 +0700

    (Bug): VoiceState avatar fetch restore

--- src/events/voiceState.ts
+++ src/events/voiceState.ts
@@ -3,7 +3,6 @@ import type client from '../index'
 
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
-const axios = require('axios');
 
 export = (client: client) => {
     try {
@@ -21,84 +20,74 @@ export = (client: client) => {
 
             const _guild = await client.guilds.fetch(`${guild?.guild_id}`)
             const _channel = await _guild.channels.fetch(`${guild?.log_id}`)
-
-            axios.get(`https://discord.com/api/guilds/${newState.guild.id}/members/${user?.id}`,
-                {
-                    headers: {
-                        Authorization: `Bot ${client.token}`
-                    }
-                }
-            ).then((res:any) => {
-                let userDisplayAvatar: string = `https://cdn.discordapp.com/guilds/${newState.guild.id}/users/${user?.id}/avatars/${res.data.avatar}.webp?size=2048`  as string;
-                if (oldState.channelId === null && newState.channelId !== null) {
-                    if (!_channel) return;
-                    if (_channel.type !== ChannelType.GuildText) return;
-                    const guildName = newState.guild.name;
-                    console.log(`[${guildName}][in-out]: ✅ ${user?.displayName} joined ${newState.channel?.name}`);
-                    _channel?.send({
-                        embeds: [
-                            {
-                                author: {
-                                    name: `${user?.displayName}`,
-                                    icon_url: `${userDisplayAvatar}`,
-                                },
-                                thumbnail: {
-                                    url: `${userDisplayAvatar}`,
-                                },
-                                color: 0x2CE51F,
-                                title: '✅ ***Member joined***',
-                                description: `**${user?.displayName}** joined **${newState.channel?.name}**`,
-                                timestamp: new Date().toISOString(),
-                            }
-                        ]
-                    })
-                } else if (oldState.channelId !== null && newState.channelId === null) {
-                    if (!_channel) return;
-                    if (_channel.type !== ChannelType.GuildText) return;
-                    const guildName = newState.guild.name;
-                    console.log(`[${guildName}][in-out]: ⭕️ ${user?.displayName} left ${oldState.channel?.name}`);
-                    _channel.send({
-                        embeds: [
-                            {
-                                author: {
-                                    name: `${user?.displayName}`,
-                                    icon_url: `${userDisplayAvatar}`,
-                                },
-                                thumbnail: {
-                                    url: `${userDisplayAvatar}`,
-                                },
-                                color: 0xB6B6B6,
-                                title: '⭕️ ***Member left***',
-                                description: `**${user?.displayName}** left **${oldState.channel?.name}**`,
-                                timestamp: new Date().toISOString(),
-                            }
-                        ]
-                    })
-                } else if (oldState.channelId !== newState.channelId) {
-                    if (!_channel) return;
-                    if (_channel.type !== ChannelType.GuildText) return;
-                    const guildName = newState.guild.name;
-                    console.log(`[${guildName}][in-out]: 🔄 ${user?.displayName} moved from ${oldState.channel?.name} to ${newState.channel?.name}`);
-                    _channel.send({
-                        embeds: [
-                            {
-                                author: {
-                                    name: `${user?.displayName}`,
-                                    icon_url: `${userDisplayAvatar}`,
-                                },
-                                thumbnail: {
-                                    url: `${userDisplayAvatar}`,
-                                },
-                                color: 0xFFC200,
-                                title: '🔄 ***Member moved***',
-                                description: `**${user?.displayName}** moved from **${oldState.channel?.name}** to **${newState.channel?.name}**`,
-                                timestamp: new Date().toISOString(),
-                            }
-                        ]
-                    })
-                }
-            });
-        });
+            if (oldState.channelId === null && newState.channelId !== null) {
+                if (!_channel) return;
+                if (_channel.type !== ChannelType.GuildText) return;
+                const guildName = newState.guild.name;
+                console.log(`[${guildName}][in-out]: ✅ ${user?.displayName} joined ${newState.channel?.name}`);
+                _channel?.send({
+                    embeds: [
+                        {
+                            author: {
+                                name: `${user?.displayName}`,
+                                icon_url: `${user?.displayAvatarURL()}`,
+                            },
+                            thumbnail: {
+                                url: `${user?.displayAvatarURL()}`,
+                            },
+                            color: 0x2CE51F,
+                            title: '✅ ***Member joined***',
+                            description: `**${user?.displayName}** joined **${newState.channel?.name}**`,
+                            timestamp: new Date().toISOString(),
+                        }
+                    ]
+                })
+            } else if (oldState.channelId !== null && newState.channelId === null) {
+                if (!_channel) return;
+                if (_channel.type !== ChannelType.GuildText) return;
+                const guildName = newState.guild.name;
+                console.log(`[${guildName}][in-out]: ⭕️ ${user?.displayName} left ${oldState.channel?.name}`);
+                _channel.send({
+                    embeds: [
+                        {
+                            author: {
+                                name: `${user?.displayName}`,
+                                icon_url: `${user?.displayAvatarURL()}`,
+                            },
+                            thumbnail: {
+                                url: `${user?.displayAvatarURL()}`,
+                            },
+                            color: 0xB6B6B6,
+                            title: '⭕️ ***Member left***',
+                            description: `**${user?.displayName}** left **${oldState.channel?.name}**`,
+                            timestamp: new Date().toISOString(),
+                        }
+                    ]
+                })
+            } else if (oldState.channelId !== newState.channelId) {
+                if (!_channel) return;
+                if (_channel.type !== ChannelType.GuildText) return;
+                const guildName = newState.guild.name;
+                console.log(`[${guildName}][in-out]: 🔄 ${user?.displayName} moved from ${oldState.channel?.name} to ${newState.channel?.name}`);
+                _channel.send({
+                    embeds: [
+                        {
+                            author: {
+                                name: `${user?.displayName}`,
+                                icon_url: `${user?.displayAvatarURL()}`,
+                            },
+                            thumbnail: {
+                                url: `${user?.displayAvatarURL()}`,
+                            },
+                            color: 0xFFC200,
+                            title: '🔄 ***Member moved***',
+                            description: `**${user?.displayName}** moved from **${oldState.channel?.name}** to **${newState.channel?.name}**`,
+                            timestamp: new Date().toISOString(),
+                        }
+                    ]
+                })
+            }
+        })
     } catch (e) {
         return;
     }
