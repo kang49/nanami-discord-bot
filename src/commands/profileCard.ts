786d3e6bc2b50f803f3c40c96d8817c1498b234e
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-07-09 17:17:59 +0700

    (Bug, Comand): chage avatar to native func

--- src/commands/profileCard.ts
+++ src/commands/profileCard.ts
@@ -47,192 +47,185 @@ export = {
                 }
             })
             if (!user_sql) return;
-            axios.get(`https://discord.com/api/guilds/${guildId}/members/${userId}`,
+            //@ts-ignore
+            let userAvatar: string = userOption.member?.displayAvatarURL({ size: 4096 })  as string;
+            const guildName: string = user_sql.guildName as string;
+
+            var userLevel: any = Math.floor(user_sql.guildMsgCount as number / 10)
+            if (userLevel >= 100) {
+                var userLevel: any = 'MAX' as string;
+            }
+
+            const userName: string = user_sql.userName as string;
+            let userTag: string = `#${user_sql.userTag}` as string;
+            if (userTag === '#0') {userTag = ''};
+            const userFullName: string = `${userName}${userTag}`
+            const userFlags = userOption.user?.flags;
+
+            //@ts-ignore
+            const userJoinDate = userOption.member?.joinedTimestamp;
+            const joinDate = new Date(userJoinDate??'');
+            const userCreateDate = userOption.user?.createdTimestamp;
+            const createDate = new Date(userCreateDate??'')
+            const userFormattedJoinDate = joinDate.toLocaleDateString('en-GB'); // 'dd/mm/yyyy' format
+            const userFormattedCreareDate = createDate.toLocaleDateString('en-GB'); // 'dd/mm/yyyy' format
+            
+            const guildMember = await interaction.guild?.members.fetch(userId);
+            const userRoles = guildMember?.roles.cache;
+            const sortedRoles = userRoles?.sort((a, b) => b.rawPosition - a.rawPosition);
+            let roleNameList: string[] = [];
+            sortedRoles?.forEach((role) => {
+                roleNameList.push(role.name);
+            });
+            
+            //get png avatar
+            userAvatar = userAvatar.replace('.webp', '.png');
+            
+            //Check UserBanner for check user has Nitro
+            axios.get(`https://discord.com/api/users/${userId}`,
                 {
                     headers: {
                         Authorization: `Bot ${client.token}`
                     }
                 }
-            ).then( async (res:any) => {
-                let userAvatar: string = `https://cdn.discordapp.com/guilds/${guildId}/users/${userId}/avatars/${res.data.avatar}.webp?size=4096`  as string;
-                const guildName: string = user_sql.guildName as string;
+            ).then((res:any) => {
+                const {banner} = res.data;
 
-                var userLevel: any = Math.floor(user_sql.guildMsgCount as number / 10)
-                if (userLevel >= 100) {
-                    var userLevel: any = 'MAX' as string;
-                }
+                //Create Canvas
+                const canvas = createCanvas(800, 600);
+                const ctx = canvas.getContext('2d');
 
-                const userName: string = user_sql.userName as string;
-                let userTag: string = `#${user_sql.userTag}` as string;
-                if (userTag === '#0') {userTag = ''};
-                const userFullName: string = `${userName}${userTag}`
-                const userFlags = userOption.user?.flags;
+                //Load Image
+                loadImage('assets/img/ProfileCard.png').then ( async (image) => {
+                    //Set canvas size
+                    canvas.width = image.width;
+                    canvas.height= image.height;
 
-                //@ts-ignore
-                const userJoinDate = userOption.member?.joinedTimestamp;
-                const joinDate = new Date(userJoinDate??'');
-                const userCreateDate = userOption.user?.createdTimestamp;
-                const createDate = new Date(userCreateDate??'')
-                const userFormattedJoinDate = joinDate.toLocaleDateString('en-GB'); // 'dd/mm/yyyy' format
-                const userFormattedCreareDate = createDate.toLocaleDateString('en-GB'); // 'dd/mm/yyyy' format
-                
-                const guildMember = await interaction.guild?.members.fetch(userId);
-                const userRoles = guildMember?.roles.cache;
-                const sortedRoles = userRoles?.sort((a, b) => b.rawPosition - a.rawPosition);
-                let roleNameList: string[] = [];
-                sortedRoles?.forEach((role) => {
-                    roleNameList.push(role.name);
-                });
-                
-                //get png avatar
-                userAvatar = userAvatar.replace('.webp', '.png');
-                
-                //Check UserBanner for check user has Nitro
-                axios.get(`https://discord.com/api/users/${userId}`,
-                    {
-                        headers: {
-                            Authorization: `Bot ${client.token}`
-                        }
-                    }
-                ).then((res:any) => {
-                    const {banner} = res.data;
+                    //Draw Image
+                    ctx.drawImage(image, 0, 0);
 
-                    //Create Canvas
-                    const canvas = createCanvas(800, 600);
-                    const ctx = canvas.getContext('2d');
+                    //Set text color and font
+                    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+                    ctx.font = 'bold 40px Arial';
+                    // Draw text at the centered position
+                    ctx.fillText(userFullName, 280, 130);
 
-                    //Load Image
-                    loadImage('assets/img/ProfileCard.png').then ( async (image) => {
-                        //Set canvas size
-                        canvas.width = image.width;
-                        canvas.height= image.height;
+                    ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
+                    ctx.font = 'bold 28.5px Arial';
+                    ctx.fillText(roleNameList[0], 280, 160);
 
-                        //Draw Image
-                        ctx.drawImage(image, 0, 0);
+                    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+                    ctx.font = 'bold 30px Arial';
+                    var TextWidth = ctx.measureText(guildName).width;
+                    // Calculate X position for centering text
+                    var centerX = (canvas.width - TextWidth) / 2;
+                    ctx.fillText(guildName, centerX-150, 290);
 
-                        //Set text color and font
-                        ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
-                        ctx.font = 'bold 40px Arial';
-                        // Draw text at the centered position
-                        ctx.fillText(userFullName, 280, 130);
+                    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+                    ctx.font = 'bold 30px Arial';
+                    var TextWidth = ctx.measureText(`ID: ${userId}`).width;
+                    // Calculate X position for centering text
+                    var centerX = (canvas.width - TextWidth) / 2;
+                    ctx.fillText(`ID: ${userId}`, centerX-170, 350);
 
-                        ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-                        ctx.font = 'bold 28.5px Arial';
-                        ctx.fillText(roleNameList[0], 280, 160);
+                    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+                    ctx.font = 'bold 30px Arial';
+                    var TextWidth = ctx.measureText(`Join: ${userFormattedJoinDate}`).width;
+                    // Calculate X position for centering text
+                    var centerX = (canvas.width - TextWidth) / 2;
+                    ctx.fillText(`Join: ${userFormattedJoinDate}`, centerX-145, 410);
 
-                        ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-                        ctx.font = 'bold 30px Arial';
-                        var TextWidth = ctx.measureText(guildName).width;
-                        // Calculate X position for centering text
-                        var centerX = (canvas.width - TextWidth) / 2;
-                        ctx.fillText(guildName, centerX-150, 290);
+                    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+                    ctx.font = 'bold 30px Arial';
+                    var TextWidth = ctx.measureText(`Create: ${userFormattedCreareDate}`).width;
+                    // Calculate X position for centering text
+                    var centerX = (canvas.width - TextWidth) / 2;
+                    ctx.fillText(`Create: ${userFormattedCreareDate}`, centerX-250, 470);
 
-                        ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-                        ctx.font = 'bold 30px Arial';
-                        var TextWidth = ctx.measureText(`ID: ${userId}`).width;
-                        // Calculate X position for centering text
-                        var centerX = (canvas.width - TextWidth) / 2;
-                        ctx.fillText(`ID: ${userId}`, centerX-170, 350);
+                    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+                    ctx.font = 'bold 50px Arial';
+                    var TextWidth = ctx.measureText(`Level`).width;
+                    // Calculate X position for centering text
+                    var centerX = (canvas.width - TextWidth) / 2;
+                    ctx.fillText(`Level`, centerX+295, 370);
 
-                        ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-                        ctx.font = 'bold 30px Arial';
-                        var TextWidth = ctx.measureText(`Join: ${userFormattedJoinDate}`).width;
-                        // Calculate X position for centering text
-                        var centerX = (canvas.width - TextWidth) / 2;
-                        ctx.fillText(`Join: ${userFormattedJoinDate}`, centerX-145, 410);
-
-                        ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-                        ctx.font = 'bold 30px Arial';
-                        var TextWidth = ctx.measureText(`Create: ${userFormattedCreareDate}`).width;
-                        // Calculate X position for centering text
-                        var centerX = (canvas.width - TextWidth) / 2;
-                        ctx.fillText(`Create: ${userFormattedCreareDate}`, centerX-250, 470);
-
-                        ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-                        ctx.font = 'bold 50px Arial';
-                        var TextWidth = ctx.measureText(`Level`).width;
-                        // Calculate X position for centering text
-                        var centerX = (canvas.width - TextWidth) / 2;
-                        ctx.fillText(`Level`, centerX+295, 370);
-
-                        ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-                        ctx.font = 'bold 100px Arial';
-                        var TextWidth = ctx.measureText(userLevel).width;
-                        // Calculate X position for centering text
-                        var centerX = (canvas.width - TextWidth) / 2;
-                        ctx.fillText(userLevel, centerX+295, 470);
+                    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+                    ctx.font = 'bold 100px Arial';
+                    var TextWidth = ctx.measureText(userLevel).width;
+                    // Calculate X position for centering text
+                    var centerX = (canvas.width - TextWidth) / 2;
+                    ctx.fillText(userLevel, centerX+295, 470);
 
 
-                        // Badge drawer
-                        if (userFlags?.has('ActiveDeveloper')) {
-                            //Badge
-                            // Load image
-                            const badgeImage = await loadImage('assets/img/active_developer_icon.png');
-                            
-                            // Calculate avatar position and radius
-                            const badgeSize = 250;
-                            const badgeX = 650;
-                            const badgeY = -10;
-
-                            // Draw avatar image within the clipping path
-                            ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
-                        } else if (userFlags?.has('PremiumEarlySupporter') || (banner)) {
-                            //Badge
-                            // Load image
-                            const badgeImage = await loadImage('assets/img/nitro_icon.png');
-                            
-                            // Calculate avatar position and radius
-                            const badgeSize = 250;
-                            const badgeX = 650;
-                            const badgeY = -10;
-                            
-                            // Draw avatar image within the clipping path
-                            ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
-                        } else if (userOption.user?.bot) {
-                            //Badge
-                            // Load image
-                            const badgeImage = await loadImage('assets/img/bot_icon.png');
-                            
-                            // Calculate avatar position and radius
-                            const badgeSize = 200;
-                            const badgeX = 670;
-                            const badgeY = 0;
-
-                            // Draw avatar image within the clipping path
-                            ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
-                        }
-
-                        
-                        // Load avatar image
-                        const avatar = await loadImage(userAvatar);
+                    // Badge drawer
+                    if (userFlags?.has('ActiveDeveloper')) {
+                        //Badge
+                        // Load image
+                        const badgeImage = await loadImage('assets/img/active_developer_icon.png');
                         
                         // Calculate avatar position and radius
-                        const avatarSize = 202;
-                        const avatarX = 44;
-                        const avatarY = 32;
-                        const radius = avatarSize / 2;
-
-                        // Create clipping path for the circle
-                        ctx.beginPath();
-                        ctx.arc(avatarX + radius, avatarY + radius, radius, 0, Math.PI * 2, true);
-                        ctx.closePath();
-                        ctx.clip();
+                        const badgeSize = 250;
+                        const badgeX = 650;
+                        const badgeY = -10;
 
                         // Draw avatar image within the clipping path
-                        ctx.drawImage(avatar, avatarX, avatarY, avatarSize, avatarSize);
+                        ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
+                    } else if (userFlags?.has('PremiumEarlySupporter') || (banner)) {
+                        //Badge
+                        // Load image
+                        const badgeImage = await loadImage('assets/img/nitro_icon.png');
+                        
+                        // Calculate avatar position and radius
+                        const badgeSize = 250;
+                        const badgeX = 650;
+                        const badgeY = -10;
+                        
+                        // Draw avatar image within the clipping path
+                        ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
+                    } else if (userOption.user?.bot) {
+                        //Badge
+                        // Load image
+                        const badgeImage = await loadImage('assets/img/bot_icon.png');
+                        
+                        // Calculate avatar position and radius
+                        const badgeSize = 200;
+                        const badgeX = 670;
+                        const badgeY = 0;
 
-                        // Save image
-                        const profileCardBuffer = canvas.toBuffer();
-                        // fs.writeFileSync('assets/test.png', profileCardBuffer);
+                        // Draw avatar image within the clipping path
+                        ctx.drawImage(badgeImage, badgeX, badgeY, badgeSize, badgeSize);
+                    }
 
-                        //Send
-                        const attachment = new Discord.AttachmentBuilder(profileCardBuffer, { name: 'ProfileCard.png' });
+                    
+                    // Load avatar image
+                    const avatar = await loadImage(userAvatar);
+                    
+                    // Calculate avatar position and radius
+                    const avatarSize = 202;
+                    const avatarX = 44;
+                    const avatarY = 32;
+                    const radius = avatarSize / 2;
 
-                        interaction.followUp({
-                            content: `**${userFullName}** ได้สร้าง **Profile Card** ของ **${guildName} Server**`,
-                            files: [attachment],
-                        })
-                    });
+                    // Create clipping path for the circle
+                    ctx.beginPath();
+                    ctx.arc(avatarX + radius, avatarY + radius, radius, 0, Math.PI * 2, true);
+                    ctx.closePath();
+                    ctx.clip();
+
+                    // Draw avatar image within the clipping path
+                    ctx.drawImage(avatar, avatarX, avatarY, avatarSize, avatarSize);
+
+                    // Save image
+                    const profileCardBuffer = canvas.toBuffer();
+                    // fs.writeFileSync('assets/test.png', profileCardBuffer);
+
+                    //Send
+                    const attachment = new Discord.AttachmentBuilder(profileCardBuffer, { name: 'ProfileCard.png' });
+
+                    interaction.followUp({
+                        content: `**${userFullName}** ได้สร้าง **Profile Card** ของ **${guildName} Server**`,
+                        files: [attachment],
+                    })
                 });
             });
         });
