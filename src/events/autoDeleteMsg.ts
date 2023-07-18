2934063c1c7ae927f36d293a16ce709eb6bd2269
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-07-18 17:59:18 +0700

    (Func, Improving): autoDelMsg Parallel Delete

--- src/events/autoDeleteMsg.ts
+++ src/events/autoDeleteMsg.ts
@@ -6,7 +6,13 @@ const prisma = new PrismaClient();
 export = (client: client) => {
     client.once('ready', async () => {
         let funcIsRuning: boolean = false as boolean;
+
+
         async function AutoDeleteMsg() {
+            //Worker running Management
+            let work0_IsOn = false;
+            let work1_IsOn = false;
+
             //check channel is set to autoDelete
             const auDelCh = await prisma.autoDeleteMsg.findMany()
             if (auDelCh.length === 0) return; //กรณีไม่มีใครตั้งค่า autoDelMsg ให้ return ทิ้ง
@@ -21,46 +27,114 @@ export = (client: client) => {
                     }
                 });
                 if (auDelMsg.length === 0) continue; //กรณีไม่มี Msg ให้ลบ ให้ไปหา guild ถัดไป
-                for (let ii = 0; ii < auDelMsg.length; ii++) {
-                    let msgCreateTime: Date = auDelMsg[ii].create_time  as Date;
-                    let msgId: string = auDelMsg[ii].messageId as string;
 
-                    if (msgCreateTime) {
-                        let currentTime = new Date();
-                        let timeDifference = currentTime.getTime() - msgCreateTime.getTime();
+                //Parallel Management
+                let work0_Value: number = auDelMsg.length / 2 as number;
+                let work1_Value: number = work0_Value as number;
 
-                        timeDifference = timeDifference / 1000 //convert nanoSec to Sec
+                if  (work0_Value % 1 === 0.5) {
+                    work0_Value + 0.5;
+                    work1_Value - 0.5;
+                }
 
-                        // check this msg is overLimit or not
-                        if (timeDifference < auDelTimeLimit) continue;
-                        else {
-                            try {
-                                // Delete the message using the message's ID
-                                //@ts-ignore
-                                await client.channels.cache.get(auDelChannelId)?.messages.delete(msgId);
-                                
-                                await prisma.deleteMessage.deleteMany({
-                                    where: {
+                const work0_auDelMsg = auDelMsg.slice(0, work0_Value);
+                const work1_auDelMsg = auDelMsg.slice(work1_Value);
+
+                const work0 = new Promise<void>(async (resolve) => {
+                    if (!work0_IsOn) {
+                        work0_IsOn = true;
+                        for (let ii_work0 = 0; ii_work0 < work0_auDelMsg.length; ii_work0++) {
+                            let msgCreateTime: Date = work0_auDelMsg[ii_work0].create_time  as Date;
+                            let msgId: string = work0_auDelMsg[ii_work0].messageId as string;
+
+                            if (msgCreateTime) {
+                                let currentTime = new Date();
+                                let timeDifference = currentTime.getTime() - msgCreateTime.getTime();
+
+                                timeDifference = timeDifference / 1000 //convert nanoSec to Sec
+
+                                // check this msg is overLimit or not
+                                if (timeDifference < auDelTimeLimit) continue;
+                                else {
+                                    try {
+                                        // Delete the message using the message's ID
                                         //@ts-ignore
-                                        messageId: msgId,
-                                    },
-                                });
-                            } catch (e) {
-                                try {
-                                    await prisma.deleteMessage.deleteMany({
-                                        where: {
-                                            //@ts-ignore
-                                            messageId: msgId,
-                                        },
-                                    });
-                                } catch {
-                                    console.log(e, 'autoDelMsg')
-                                    continue;
-                                }
+                                        await client.channels.cache.get(auDelChannelId)?.messages.delete(msgId);
+                                        
+                                        await prisma.deleteMessage.deleteMany({
+                                            where: {
+                                                //@ts-ignore
+                                                messageId: msgId,
+                                            },
+                                        });
+                                    } catch (e) {
+                                        try {
+                                            await prisma.deleteMessage.deleteMany({
+                                                where: {
+                                                    //@ts-ignore
+                                                    messageId: msgId,
+                                                },
+                                            });
+                                        } catch {
+                                            console.log(e, 'autoDelMsg')
+                                            continue;
+                                        }
+                                    };
+                                };
                             };
                         };
+                        work0_IsOn = false;
                     };
-                };
+                    resolve();
+                });
+                const work1 = new Promise<void>(async (resolve) => {
+                    if (!work1_IsOn) {
+                        work1_IsOn = true;
+                        for (let ii_work1 = 0; ii_work1 < work1_auDelMsg.length; ii_work1++) {
+                            let msgCreateTime: Date = work1_auDelMsg[ii_work1].create_time  as Date;
+                            let msgId: string = work1_auDelMsg[ii_work1].messageId as string;
+
+                            if (msgCreateTime) {
+                                let currentTime = new Date();
+                                let timeDifference = currentTime.getTime() - msgCreateTime.getTime();
+
+                                timeDifference = timeDifference / 1000 //convert nanoSec to Sec
+
+                                // check this msg is overLimit or not
+                                if (timeDifference < auDelTimeLimit) continue;
+                                else {
+                                    try {
+                                        // Delete the message using the message's ID
+                                        //@ts-ignore
+                                        await client.channels.cache.get(auDelChannelId)?.messages.delete(msgId);
+                                        
+                                        await prisma.deleteMessage.deleteMany({
+                                            where: {
+                                                //@ts-ignore
+                                                messageId: msgId,
+                                            },
+                                        });
+                                    } catch (e) {
+                                        try {
+                                            await prisma.deleteMessage.deleteMany({
+                                                where: {
+                                                    //@ts-ignore
+                                                    messageId: msgId,
+                                                },
+                                            });
+                                        } catch {
+                                            console.log(e, 'autoDelMsg')
+                                            continue;
+                                        }
+                                    };
+                                };
+                            };
+                        };
+                        work1_IsOn = false;
+                    };
+                    resolve();
+                });
+                await Promise.all([work0, work1]);
             };
             return funcIsRuning = true;
         };
